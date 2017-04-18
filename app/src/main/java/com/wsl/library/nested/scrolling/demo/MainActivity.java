dfd627a9d372614d991396d9052bb477d1a44b63
doubleDragon <aiai373824745@gmail.com>
2017-04-18 17:28:13 +0800

    First commit

---
+++ app/src/main/java/com/wsl/library/nested/scrolling/demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wsl.library.nested.scrolling.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wsl.library.nested.scrolling.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
