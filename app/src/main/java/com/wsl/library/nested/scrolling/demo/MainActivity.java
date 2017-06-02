585ae11a3e4fe16867a65205218e1d8e356263f7
doubleDragon <aiai373824745@gmail.com>
2017-06-02 17:37:29 +0800

    Replace compile to provided for library

--- app/src/main/java/com/wsl/library/nested/scrolling/demo/MainActivity.java
+++ app/src/main/java/com/wsl/library/nested/scrolling/demo/MainActivity.java
@@ -3,8 +3,6 @@ package com.wsl.library.nested.scrolling.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.wsl.library.nested.scrolling.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
